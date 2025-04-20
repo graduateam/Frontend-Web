d89d2fabe9b034b96bb44676f210db7e423c798d
RealPinkRabbit <sdg0896@gmail.com>
2025-04-21 03:50:42 +0900

    feat: 관리자 로그인 페이지 뼈대 구현 #1

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="/src/assets/images/logos/circle-logo.svg"
+        alt="Circle Logo"
+        className="header-logo"
+      />
+      <div className="header-title">Smart Road Reflector</div>
+    </header>
+  );
+};
+
+export default Header;
