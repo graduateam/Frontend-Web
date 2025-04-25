e2dd326a8705907fa49fc31ee435a08bbfd1d5f0
RealPinkRabbit <sdg0896@gmail.com>
2025-04-25 12:13:12 +0900

    doc: mainpage.css를 global.css에 병합

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,7 +3,6 @@ import Header from "../components/layout/Header";
 import NaverMap from "../components/map/NaverMap";
 import useAuth from "../hooks/useAuth";
 import { Navigate } from "react-router-dom";
-import "../assets/styles/mainpage.css"; // 새 스타일 파일 추가
 
 const MainPage = () => {
   const { isAuthenticated } = useAuth();
